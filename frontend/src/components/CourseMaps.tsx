36d67632455098ea42883a7f617c8628ddfd9828
Josh Coles <josh@joshcoles.com>
2024-08-15 19:30:55 -0300

    Begin work to fetch thumbnails and build out about page.

--- frontend/src/components/CourseMaps.tsx
+++ frontend/src/components/CourseMaps.tsx
@@ -6,13 +6,20 @@ const CourseMaps: React.FC = () => {
 	const [courseMapUrls, setCourseMapUrls] = useState<string[]>([]);
 	const [isLightboxOpen, setLightboxOpen] = useState(false);
 	const [loading, setLoading] = useState<boolean>(true);
+	const [largeImageUrl, setLargeImageUrl] = useState<string | undefined>(
+		undefined,
+	);
+	const [largeImageLoading, setLargeImageLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		const fetchMapUrl = async () => {
 			try {
 				const urls = [];
-				for (let i = 1; i <= 13; i++) {
-					const mapRef = ref(storage, `course-maps/hole-${i}.webp`);
+				for (let i = 1; i <= 18; i++) {
+					const mapRef = ref(
+						storage,
+						`course-maps/hole-${i}-thumbnail.webp`,
+					);
 					const url = await getDownloadURL(mapRef);
 					urls.push(url);
 				}
@@ -28,24 +35,50 @@ const CourseMaps: React.FC = () => {
 		fetchMapUrl();
 	}, []);
 
+	const fetchLargeImage = async (index: number) => {
+		try {
+			setLargeImageLoading(true);
+
+			const mapRef = ref(storage, `course-maps/hole-${index + 1}.webp`);
+			const url = await getDownloadURL(mapRef);
+			setLargeImageUrl(url);
+		} catch (err) {
+			console.error(err);
+		} finally {
+			setLargeImageLoading(false);
+		}
+	};
+
 	if (loading) {
 		return <div>Loading...</div>;
 	}
 
 	return (
 		<div>
-			<button onClick={() => setLightboxOpen((prev) => !prev)}>
-				Open Lightbox
-			</button>
 			<LightBox
 				isOpen={isLightboxOpen}
 				onClose={() => setLightboxOpen((prev) => !prev)}
-				content={<h1>Lightbox</h1>}
+				content={
+					largeImageLoading ? (
+						<div>Loading large image...</div>
+					) : (
+						largeImageUrl && (
+							<img src={largeImageUrl} alt="Large course map" />
+						)
+					)
+				}
 			/>
 			<ul className="flex flex-wrap">
 				{courseMapUrls.map((url, index) => (
-					<li key={index}>
-						<img className="h-60 mr-1 mb-1" src={url} />
+					<li
+						onClick={() => {
+							setLightboxOpen(true);
+							fetchLargeImage(index);
+						}}
+						className="bg-green-primary p-2 m-2 cursor-pointer"
+						key={index}
+					>
+						<img className="h-60 min-h-60" src={url} />
 					</li>
 				))}
 			</ul>
--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -3,7 +3,64 @@ import CourseMaps from '../components/CourseMaps';
 
 const AboutPage: React.FC = () => {
 	return (
-		<div className="h-screen">
+		<div className="bg-yellow-primary px-5">
+			<h2 className="font-headline text-center py-5">About Us</h2>
+			<div className="flex flex-col">
+				<div className="">
+					<img
+						src="https://via.placeholder.com/600x400"
+						className=""
+					/>
+				</div>
+				<p className="">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+					vel urna a augue malesuada convallis. Fusce lacinia lectus
+					eget sapien varius, nec aliquam massa bibendum. Donec ac sem
+					sit amet tortor convallis venenatis. Aliquam erat volutpat.
+					Etiam ut bibendum eros. Nulla facilisi. Duis euismod nisi
+					nec nisl vehicula, a tempus arcu commodo.
+				</p>
+			</div>
+
+			<div className="">
+				<p className="text-lg text-gray-700 leading-relaxed">
+					Pellentesque habitant morbi tristique senectus et netus et
+					malesuada fames ac turpis egestas. Curabitur nec vestibulum
+					lorem, in pharetra erat. Aenean sodales magna sit amet quam
+					feugiat, in interdum massa gravida. Sed fringilla dapibus
+					eros, sit amet luctus velit blandit id. Morbi tincidunt urna
+					in orci suscipit, sed convallis risus aliquam. In a faucibus
+					sapien. Vivamus vitae massa purus. convallis id.
+				</p>
+				<div className="">
+					<img
+						src="https://via.placeholder.com/600x400"
+						alt="Secondary Image 1"
+						className="rounded-lg shadow-lg"
+					/>
+				</div>
+			</div>
+
+			<div className="flex flex-col lg:flex-row items-center">
+				<div className="">
+					<p className="text-lg text-gray-700 leading-relaxed">
+						Sed malesuada ex a velit tincidunt, vel efficitur ex
+						suscipit. Nulla id sapien a sapien ultrices sollicitudin
+						non sit amet enim. Nullam a lacinia sem, sit amet
+						lacinia libero. Duis convallis nisi non tellus vehicula,
+						vel placerat justo condimentum. Praesent at urna at
+						velit tristique lacinia.
+					</p>
+				</div>
+				<div className="">
+					<img
+						src="https://via.placeholder.com/600x400"
+						alt="Secondary Image 2"
+						className="rounded-lg shadow-lg"
+					/>
+				</div>
+			</div>
+			<h2>The Course</h2>
 			<CourseMaps />
 		</div>
 	);
