f69bfaf1c6b5cfdd18be2a7249977eca1a47514f
Josh Coles <josh@joshcoles.com>
2024-09-19 12:42:03 -0300

    Adjust about section content.

--- frontend/src/components/about/AboutSection.tsx
+++ frontend/src/components/about/AboutSection.tsx
@@ -3,9 +3,6 @@ import React from 'react';
 const AboutSection: React.FC = () => {
 	return (
 		<>
-			<h2 className="text-brown-primary mb-5 text-center text-4xl">
-				About The Course
-			</h2>
 			<div className="flex justify-center mb-8">
 				<img
 					src="/overhead.jpg"
@@ -15,20 +12,20 @@ const AboutSection: React.FC = () => {
 			</div>
 			<div className="mb-4">
 				<p className="mb-5 font-bold">
-					Glenaladale Disc Golf Course opened in June of 2022. It
-					operates as a separate entity from the Glenaladale Heritage
-					Trust, from which it leases the course's land. The course is
-					grateful to the trust for working with them on this
-					ambitious project, and for letting them share their space!
+					Glenaladale Disc Golf Course opened in June of 2022. It is a
+					separate entity from the Glenaladale Heritage Trust, from
+					which it leases the course's land. The course is grateful to
+					the trust for their help and for letting them share their
+					space!
 				</p>
 				<p className="mb-5">
 					The course was designed and built by course owners /
 					operators <strong>Josh Coles</strong>,{' '}
 					<strong>Andrew Clark</strong> and{' '}
-					<strong>Patrick Callbeck</strong>. The three friends are
-					avid disc golfers and wanted to try their hand at adding to
-					the list of world-class destination disc golf courses on
-					PEI, and to create one with a strong community.
+					<strong>Patrick Callbeck</strong>. The three are close
+					friends and avid disc golfers. and wanted to try their hand
+					at adding to the list of world-class destination disc golf
+					courses on PEI, and to create one with a strong community.
 				</p>
 				<p className="mb-5">
 					At approximately 7,000ft long from the red layout and
@@ -53,37 +50,35 @@ const AboutSection: React.FC = () => {
 							course owners.
 						</p>
 						<p className="mb-2">
-							Andrew is a climate data and policy specialist from
-							Charlottetown, working with the government of PEI.
-							He has background in GIS and was part of the UPEI
-							Climate Lab team that initially built CLIVE (CoastaL
-							Impacts Visualization Environment). He started
-							playing disc golf around 2013, and quickly became
-							interested in travelling around the maritimes
-							competing in tournaments.
+							<strong>Andrew</strong> is a climate data and policy
+							specialist from Charlottetown, working with the
+							government of PEI. He has background in GIS and was
+							part of the UPEI Climate Lab team that initially
+							built CLIVE (CoastaL Impacts Visualization
+							Environment). He started playing disc golf around
+							2013, and quickly became interested in travelling
+							around the maritimes competing in tournaments.
 						</p>
 						<p className="mb-2">
-							Josh is a software engineer from Charlottetown. He
-							began playing disc golf in 2011 when his friend Jeff
-							introduced him to Hillcrest Farm, which had just
-							opened. He continued to play casually after moving
-							to Vancouver and then Toronto, where he became
-							interested in the competitive side of Disc Golf. His
-							moving back to PEI coincided with the development
-							and opening of the course.
+							<strong>Josh</strong> is a software engineer from
+							Charlottetown. He began playing disc golf in 2011
+							when his friend Jeff introduced him to Hillcrest
+							Farm, which had just opened. He continued to play
+							casually after moving to Vancouver and then Toronto,
+							where he became interested in the competitive side
+							of Disc Golf. His moving back to PEI coincided with
+							the development and opening of the course.
 						</p>
 						<p className="mb-2">
-							Patrick is a carpenter from Charlottetown
-							specializing in finish carpentry and custom
-							cabinetry, which he learned largely while living and
-							working in Montreal. Upon moving back to PEI in 2020
-							he got a job restoring the Glenaladale schoolhouse,
-							which now acts as a central hub for the Glenaladale
-							Heritage Trust. After playing a handful of rounds of
-							disc golf in the 2010s, he was introduced to the
-							sport in 2020 by Josh and like many "covid disc
-							golfers", was instantly hooked and quickly caught up
-							to more experienced golfers.
+							<strong>Patrick</strong> is a carpenter from
+							Charlottetown specializing in finish carpentry and
+							custom cabinetry. Upon moving back to PEI in 2020 he
+							got a job restoring the Glenaladale schoolhouse.
+							After playing a handful of rounds of disc golf in
+							the 2010s, he was introduced to the sport in 2020 by
+							Josh and like many "covid disc golfers", was
+							instantly hooked and quickly caught up to more
+							experienced golfers.
 						</p>
 					</div>
 					<div className="mt-4 md:mt-0 md:w-1/2">
@@ -96,8 +91,7 @@ const AboutSection: React.FC = () => {
 							Clockwise from top left: Patrick Callbeck, Andrew
 							Clark, Alexis Bulman, Josh Coles. Alexis created
 							much of the initial signage of the course and
-							contributes to maintenance and beutification on a
-							regular basis.
+							contributes to its maintenance and beautification.
 						</figcaption>
 					</div>
 				</div>
@@ -113,7 +107,7 @@ const AboutSection: React.FC = () => {
 							Trust, and is across the road from the historic
 							Glenaladale Estate. Before that it was home to
 							Mi’kmaq, followed by the Acadians, the 1772
-							Glenaladale Scots, and later the Mackinnnon family.
+							Glenaladale Scots, and later the MacKinnnon family.
 							For more information, please visit the
 							<a
 								className="p-1 text-brown-950 underline"
@@ -125,17 +119,16 @@ const AboutSection: React.FC = () => {
 							.
 						</p>
 						<p className="mb-2">
-							Much of the course is built on sections of forest
-							which have been harvested as recently as about 2015.
-							These sections were slightly easier to develop into
-							grassy fairways than the holes which were cut out of
-							mature forest. We also have holes that play along
-							the edge of an active farmer's field, as well as
-							holes which play through an established tree
-							plantation with neatly organized rows of trees,
-							providing "natural" disc golf fairways.
+							Before the course, this land was forested as
+							recently as about 2015. These sections were slightly
+							easier to develop into grassy fairways than the
+							holes which were cut out of mature forest. We also
+							have holes that play along the edge of an active
+							farmer's field, as well as holes which play through
+							an established tree plantation with neatly organized
+							rows of trees, providing "natural" disc golf
+							fairways.
 						</p>
-
 						<p>
 							In September of 2022 the course was hit by Hurricane
 							Fiona, which carried winds of approximately 150
@@ -178,20 +171,16 @@ const AboutSection: React.FC = () => {
 							Schoolhouse, which now sits prominently at the
 							entrance to the estate.
 						</p>
-
 						<p className="mb-2">
 							At the time, the heritage trust was looking for ways
 							to extend the reach of the estate into the
 							community, in particular to a younger demographic
 							than what the estate was typically frequented by.
-							Having recently been re-introduced to disc golf by
-							Josh, Patrick pitched the idea of installing a
-							course on the property and brought Josh into the
-							conversation. Josh suggested including Andrew,
-							mutual friend who had previously expressed interest
-							in building a course elsewhere on PEI.
+							Patrick got Josh involved, who got Andrew involved.
+							They pitched the idea of installing a course on the
+							property to the Glenaladale Heritage Trust, who gave
+							their support.
 						</p>
-
 						<p className="mb-2">
 							The initial course build was financed largely by an
 							ACOA grant, which the heritage trust helped the disc
